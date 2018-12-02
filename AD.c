c58cbd96652b0ae9a520de3c4a5f3eb212ec7948
Cleissom Fernandes <cleissomfernandes@hotmail.com>
2018-12-02 08:29:38 -0200

    IIO Framework working

--- AD.c
+++ AD.c
@@ -5,11 +5,7 @@
 
 MODULE_AUTHOR("Cleisson Fernandes");
 MODULE_DESCRIPTION("Device Driver AD");
-//MODULE_LICENCE("GPL v2");
-
-struct AD_info {
-	int number;
-};
+MODULE_LICENSE("GPL v2");
 
 struct AD_data {
 	struct i2c_client *client;
@@ -22,14 +18,33 @@ static int AD_read_raw(struct iio_dev *indio_dev,
 			struct iio_chan_spec const *chan,
 			int * val, int * val2, long mask)
 {
-	return 0;
+	*val = 1;
+	*val2 = 2;
+	printk(KERN_INFO "AD_read_raw\n");
+	return 1;
 };
 
 static int AD_write_raw(struct iio_dev *indio_dev,
                         struct iio_chan_spec const *chan,
                         int val, int val2, long mask)
 {
+	printk(KERN_INFO "AD_write_raw\n");
 	return 0;
+};
+
+static const struct iio_chan_spec AD_channels[] = {
+	{
+	    .type = IIO_VOLTAGE,
+	    .indexed = 1,         
+	    .channel = 0,
+	    .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+	    .type = IIO_VOLTAGE,
+	    .indexed = 1,         
+	    .channel = 1,
+	    .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
 };
 
 static const struct iio_info AD_info = {
@@ -44,6 +59,8 @@ static int AD_probe(struct i2c_client *client,
 	struct AD_data *data;
 	struct iio_dev *indio_dev;
 
+	printk(KERN_INFO "AD_probe\n");
+
 	indio_dev = devm_iio_device_alloc(&client->dev, sizeof(*data));
 	if (!indio_dev)
 		return -ENOMEM;
@@ -51,14 +68,27 @@ static int AD_probe(struct i2c_client *client,
 	data = iio_priv(indio_dev);
 	i2c_set_clientdata(client, indio_dev);
 	data->client = client;
-	
-	printk(KERN_DEBUG "AD_probe");
 
-	return 0;
+	mutex_init(&data->lock);
+	indio_dev->dev.parent = &client->dev;
+	indio_dev->info = &AD_info;
+	indio_dev->name = id->name;
+	indio_dev->channels = AD_channels;
+	indio_dev->num_channels = ARRAY_SIZE(AD_channels);
+	indio_dev->modes = INDIO_DIRECT_MODE;
+	
+	return iio_device_register(indio_dev);
 };
 
 static int AD_remove(struct i2c_client *client)
 {
+	printk(KERN_INFO "AD_remove\n");
+
+	struct iio_dev *indio_dev = i2c_get_clientdata(client);
+	struct bh1750_data *data = iio_priv(indio_dev);
+
+	iio_device_unregister(indio_dev);
+
 	return 0;
 };
 
@@ -76,7 +106,5 @@ static struct i2c_driver AD_driver ={
 	.remove = AD_remove,
 	.id_table = AD_id,
 };
-//module_i2c_driver(AD_driver);
+module_i2c_driver(AD_driver);
 
-module_init(AD_probe);
-//module_exit(AD_remove);
